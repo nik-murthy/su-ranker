132059bd9eb42941124f79243c61d2e5ec09c73c
Your Name <nmurthy@icm-consulting.com.au>
2020-01-04 16:40:55 +1100

    adding content to about component

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,52 @@
 <template>
     <v-container>
-        <v-row>
-            About su-ranker-web
-        </v-row>
+        
+        <p class="text-center font-weight-bold headline">Welcome to Aussie Suburb Ranker!</p>
+            
+        
+
+            <p>
+            Aussie Suburb Ranker rates suburbs across Australia on their potential for investment properties.
+            Here is how the site works.
+            </p>
+        
+        
+            <p>Three factors are considered for ranking - </p>            
+            <p><ul>
+                <li>Population growth</li>
+                <li>Hospitals</li>
+                <li>Universities</li>
+            </ul>
+            </p>            
+            <p>
+                Click on the "SUBURBS" link at the top right corner of this page to view all suburbs across Australia
+                and to access the search and sort features.
+            </p>
+            <p class="title">Search and Sort</p>
+        
+            <p>
+                You can perform search for suburbs by price for house or unit. If you enter 400,000 and hit search,
+                you will be shown all the suburbs where the median property price is within that price. 
+            </p>
+            <p>
+                There is a switch on the left of the budget/price field. If the switch is to the left, search is 
+                performed on median house price within the number entered. If the switch is to the right, then search
+                is performed by median unit price.
+            </p>
+            <p>
+                You can filter your results based on the state of your interest. You can also sort the suburbs based
+                on their overall rank (Relevance) or based on population growth (Demographics).
+            </p>
+            <p class="title">Suburb Details</p>
+            <p>
+                Clicking on a suburb will show you the details of the suburb such as summary from Wikipedia, which hospitals
+                and universities are in the suburb and what the population growth is like.
+            </p>
+            <p>
+                You can sort and filter the results even without performing a search. The list of suburbs will include
+                all suburbs in Australia. Hope you enjoy using Aussie Suburb Ranker and thank you for visiting!
+            </p>
+    
     </v-container>
 </template>
 <script>
